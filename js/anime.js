10df554406118954e8e013ce9f8c9494f3263484
Jason Paul <jasonp@AP-LAPTOP-007.local>
2018-11-20 13:16:42 -0800

    Shana, where is the link?

---
+++ js/anime.js
@@ -0,0 +1,90 @@
+$(function() {
+  anime.timeline({loop: true})
+    .add({
+      targets: '.ml5 .line',
+      opacity: [0.5,1],
+      scaleX: [0, 1],
+      easing: "easeInOutExpo",
+      duration: 700
+    }).add({
+      targets: '.ml5 .line',
+      duration: 600,
+      easing: "easeOutExpo",
+      translateY: function(e, i, l) {
+        var offset = -0.625 + 0.625*2*i;
+        return offset + "em";
+      }
+    }).add({
+      targets: '.ml5 .ampersand',
+      opacity: [0,1],
+      scaleY: [0.5, 1],
+      easing: "easeOutExpo",
+      duration: 600,
+      offset: '-=600'
+    }).add({
+      targets: '.ml5 .letters-left',
+      opacity: [0,1],
+      translateX: ["0.5em", 0],
+      easing: "easeOutExpo",
+      duration: 600,
+      offset: '-=300'
+    }).add({
+      targets: '.ml5 .letters-right',
+      opacity: [0,1],
+      translateX: ["-0.5em", 0],
+      easing: "easeOutExpo",
+      duration: 600,
+      offset: '-=600'
+    }).add({
+      targets: '.ml5',
+      opacity: 0,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1000
+    });
+
+
+  // Wrap every letter in a span
+$('.ml7 .letters').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml7 .letter',
+    translateY: ["1.1em", 0],
+    translateX: ["0.55em", 0],
+    translateZ: 0,
+    rotateZ: [180, 0],
+    duration: 750,
+    easing: "easeOutExpo",
+    delay: function(el, i) {
+      return 50 * i;
+    }
+  }).add({
+    targets: '.ml7',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml15 .word',
+    scale: [14,1],
+    opacity: [0,1],
+    easing: "easeOutCirc",
+    duration: 800,
+    delay: function(el, i) {
+      return 800 * i;
+    }
+  }).add({
+    targets: '.ml15',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+});
---
+++ js/unbreakable.js
@@ -0,0 +1,7 @@
+var hotbod = document.querySelector(".unbreakable-container");
+
+function doStuff() {
+    hotbod.className += " animate";
+}
+
+doStuff();
